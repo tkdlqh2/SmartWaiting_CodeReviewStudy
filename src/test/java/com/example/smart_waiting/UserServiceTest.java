ac552b9420b636f3b78e6a612a205a5c7eaff8fa
tkdlqh2 <yhj7124@naver.com>
2022-09-09 02:28:00 +0900

    유저 생성 실패 test 추가

--- src/test/java/com/example/smart_waiting/UserServiceTest.java
+++ src/test/java/com/example/smart_waiting/UserServiceTest.java
@@ -6,6 +6,9 @@ import com.example.smart_waiting.user.model.UserDto;
 import com.example.smart_waiting.user.model.UserInput;
 import com.example.smart_waiting.user.repository.UserRepository;
 import com.example.smart_waiting.user.service.UserServiceImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
@@ -13,10 +16,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.BDDMockito.given;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -26,6 +31,16 @@ public class UserServiceTest {
 
     @InjectMocks
     private UserServiceImpl userService;
+
+    @BeforeEach
+    void setup(){
+        User user = User.builder()
+                .email("yhj1234@naver.com")
+                .phone("010-1111-1111")
+                .build();
+
+        given(userRepository.findById("yhj1234@naver.com")).willReturn(Optional.of(user));
+    }
 
     @Test
     void createUserSuccess(){
@@ -46,4 +61,23 @@ public class UserServiceTest {
         assertTrue(serviceResult.isSuccess());
     }
 
+    @Test
+    @DisplayName("유저 생성 실패 - 이미 등록된 이메일")
+    void createUser_UserExist(){
+        //given
+        UserInput userInput = UserInput.builder()
+                .email("yhj1234@naver.com")
+                .password("1111")
+                .phone("010-1111-2222")
+                .build();
+
+        ArgumentCaptor<User> captor = ArgumentCaptor.forClass(User.class);
+
+        //when
+        ServiceResult serviceResult = userService.createUser(userInput);
+
+        //then
+        verify(userRepository,times(0)).save(captor.capture());
+        assertFalse(serviceResult.isSuccess());
+    }
 }
