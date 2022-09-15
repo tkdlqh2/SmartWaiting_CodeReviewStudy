73525ee19e344a271db4b8720e9722e026451833
tkdlqh2 <yhj7124@naver.com>
2022-09-16 03:41:26 +0900

    TokenProvider class 추가

---
+++ src/main/java/com/example/smart_waiting/security/TokenProvider.java
@@ -0,0 +1,69 @@
+package com.example.smart_waiting.security;
+
+import com.example.smart_waiting.user.service.UserService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class TokenProvider {
+
+    private static final long TOKEN_EXPIRE_TIME = 1000*60*60*24; //1DAY
+    private static final String KEY_ROLES = "roles";
+
+    private final UserService userService;
+
+    @Value("{spring.jwt.secret}")
+    private String secretKey;
+
+    public String generateToken(String username,String roles){
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put(KEY_ROLES,roles);
+
+        var now = new Date();
+        var expiredDate = new Date(now.getTime() + TOKEN_EXPIRE_TIME);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiredDate)
+                .signWith(SignatureAlgorithm.HS512,secretKey)
+                .compact();
+    }
+
+    public Authentication getAuthentication(String jwt){
+
+        UserDetails userDetails = userService.loadUserByUsername(getUsername(jwt));
+        return new UsernamePasswordAuthenticationToken(userDetails,"",userDetails.getAuthorities());
+    }
+
+    public String getUsername(String token){
+        return this.parseClaims(token).getSubject();
+    }
+
+    public boolean validateToken(String token){
+        if(!StringUtils.hasText(token)) return false;
+
+        var claims = this.parseClaims(token);
+        return !claims.getExpiration().before(new Date());
+    }
+
+    private Claims parseClaims(String token){
+        try{
+            return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+        } catch (ExpiredJwtException e){
+            return e.getClaims();
+        }
+    }
+}
