5dc1ec20f777c1f317d0122d80ad2d229da218ce
tkdlqh2 <yhj7124@naver.com>
2022-09-20 22:58:26 +0900

    add CustomAuthenticationEntryPoint class, EntryPointErrorResponse class

---
+++ src/main/java/com/example/smart_waiting/domain/EntryPointErrorResponse.java
@@ -0,0 +1,15 @@
+package com.example.smart_waiting.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class EntryPointErrorResponse {
+
+    private String msg;
+}
