6802f056748df6b490ef6b27b069510d972249a2
tkdlqh2 <yhj7124@naver.com>
2022-09-20 17:38:32 +0900

    Exception 추가 : PasswordNotMatchException

---
+++ src/main/java/com/example/smart_waiting/exception/PasswordNotMatchException.java
@@ -0,0 +1,8 @@
+package com.example.smart_waiting.exception;
+
+public class PasswordNotMatchException extends RuntimeException{
+    private static final String MESSAGE= "비밀번호가 일치하지 않습니다.";
+
+    public PasswordNotMatchException(){super(MESSAGE);}
+
+}
