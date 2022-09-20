c6ca0d1cbede612cf5e893063b7353dcc1b778e4
tkdlqh2 <yhj7124@naver.com>
2022-09-21 02:46:04 +0900

    add: findFromRequest method 추가

--- src/main/java/com/example/smart_waiting/user/service/UserService.java
+++ src/main/java/com/example/smart_waiting/user/service/UserService.java
@@ -1,30 +1,24 @@
 package com.example.smart_waiting.user.service;
 
+import com.example.smart_waiting.domain.ServiceResult;
+import com.example.smart_waiting.user.User;
 import com.example.smart_waiting.user.model.UserDto;
 import com.example.smart_waiting.user.model.UserInput;
 import com.example.smart_waiting.user.model.UserLoginInput;
-import com.example.smart_waiting.user.model.UserPasswordResetInput;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Service
 public interface UserService extends UserDetailsService {
 
-    void createUser(UserInput userInput);
+    ServiceResult createUser(UserInput userInput);
 
     boolean existEmail(String email);
 
     boolean existPhone(String phone);
 
-    void emailAuth(String uuid);
+    ServiceResult emailAuth(String uuid);
 
     UserDto login(UserLoginInput parameter);
 
-    UserDto findFromRequest(HttpServletRequest request);
-
-    void updateInfo(String email,UserInput parameter);
-
-    void updatePassword(String email, UserPasswordResetInput parameter);
 }
--- src/main/java/com/example/smart_waiting/user/service/UserServiceImpl.java
+++ src/main/java/com/example/smart_waiting/user/service/UserServiceImpl.java
@@ -1,16 +1,14 @@
 package com.example.smart_waiting.user.service;
 
 import com.example.smart_waiting.components.MailComponents;
-import com.example.smart_waiting.exception.UserException;
-import com.example.smart_waiting.security.TokenUtil;
-import com.example.smart_waiting.type.ErrorCode;
+import com.example.smart_waiting.domain.ServiceResult;
+import com.example.smart_waiting.exception.PasswordNotMatchException;
 import com.example.smart_waiting.type.UserStatus;
 import com.example.smart_waiting.user.User;
 import com.example.smart_waiting.user.UserRepository;
 import com.example.smart_waiting.user.model.UserDto;
 import com.example.smart_waiting.user.model.UserInput;
 import com.example.smart_waiting.user.model.UserLoginInput;
-import com.example.smart_waiting.user.model.UserPasswordResetInput;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,10 +17,10 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -36,7 +34,7 @@ public class UserServiceImpl implements UserService{
 
     @Transactional
     @Override
-    public void createUser(UserInput userInput) {
+    public ServiceResult createUser(UserInput userInput) {
 
         String encryptPassword = passwordEncoder.encode(userInput.getPassword());
         String uuid = UUID.randomUUID().toString();
@@ -44,7 +42,6 @@ public class UserServiceImpl implements UserService{
         userRoleList.add("normal");
 
         User user = User.builder()
-                .name(userInput.getName())
                 .email(userInput.getEmail())
                 .password(encryptPassword)
                 .phone(userInput.getPhone())
@@ -61,6 +58,8 @@ public class UserServiceImpl implements UserService{
         String text = "<p>Smart Waiting 사이트 가입을 축하드립니다.<p><p>아래 링크를 클릭하셔서 가입을 완료 하세요.</p>"
                 + "<div><a target='_blank' href='http://localhost:8080/user/email-auth?id=" + uuid + "'> 가입 완료 </a></div>";
         mailComponents.sendMail(email, subject, text);
+
+        return ServiceResult.success();
     }
 
     @Transactional(readOnly = true)
@@ -78,13 +77,18 @@ public class UserServiceImpl implements UserService{
     // 회원 가입 신청을 넣고 인증기한이 만료된 사용자에 대한 처리 필요!
     @Transactional
     @Override
-    public void emailAuth(String uuid) {
+    public ServiceResult emailAuth(String uuid) {
+        Optional<User> optionalUser = userRepository.findByAuthKey(uuid);
+        if(optionalUser.isEmpty()){
+            // 인증키가 유효하지 않음!
+            return ServiceResult.fail("인증키가 유효하지 않습니다.");
+        }
 
-        User user = userRepository.findByAuthKey(uuid).orElseThrow(
-                ()-> new UserException(ErrorCode.REG_KEY_INVALIDATE));
+        User user = optionalUser.get();
 
         if(user.getAuthDate().isBefore(LocalDateTime.now())){
-            throw new UserException(ErrorCode.REG_KEY_EXPIRED);
+            // 만료된 인증키!
+            return ServiceResult.fail("인증키가 만료되었습니다.");
         }
 
         user.setUserStatus(UserStatus.APPROVED);
@@ -92,6 +96,7 @@ public class UserServiceImpl implements UserService{
         user.setAuthDate(null);
         userRepository.save(user);
 
+        return ServiceResult.success();
     }
 
     @Override
@@ -107,42 +112,9 @@ public class UserServiceImpl implements UserService{
                 ()-> new UsernameNotFoundException("이메일이 없습니다. -> "+parameter.getEmail()));
 
         if(!passwordEncoder.matches(parameter.getPassword(), user.getPassword())){
-            throw new UserException(ErrorCode.PASSWORD_NOT_MATCH);
+            throw new PasswordNotMatchException();
         }
         System.out.println("로그인을 하였습니다.");
         return UserDto.of(user);
-    }
-
-    @Override
-    public UserDto findFromRequest(HttpServletRequest request) {
-        String email = TokenUtil.getEmail(TokenUtil.resolveTokenFromRequest(request));
-        User user = userRepository.findByEmail(email).orElseThrow(
-                ()-> new UsernameNotFoundException("이메일이 없습니다. -> "+email));
-        return UserDto.of(user);
-    }
-
-    @Override
-    public void updateInfo(String email, UserInput parameter) {
-
-        User user = userRepository.findByEmail(email).orElseThrow(
-                ()->new UsernameNotFoundException("이메일이 없습니다. -> "+email));
-        user.setPhone(parameter.getPhone());
-        userRepository.save(user);
-    }
-
-
-    @Override
-    public void updatePassword(String email, UserPasswordResetInput parameter) {
-
-        User user = userRepository.findByEmail(email).orElseThrow(
-                ()->new UsernameNotFoundException("이메일이 없습니다. -> "+email));
-
-        if(!passwordEncoder.matches(parameter.getPassword(), user.getPassword())){
-            throw new UserException(ErrorCode.PASSWORD_NOT_MATCH);
-        }
-
-        String encryptedPassword = passwordEncoder.encode(parameter.getNewPassword());
-        user.setPassword(encryptedPassword);
-        userRepository.save(user);
     }
 }
