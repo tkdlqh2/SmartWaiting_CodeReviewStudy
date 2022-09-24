687aae58a9ee0c6995068a4a82f15033dec0c6d2
tkdlqh2 <yhj7124@naver.com>
2022-09-25 04:26:43 +0900

    Market service test class 추가

---
+++ src/test/java/com/example/smart_waiting/MarketServiceTest.java
@@ -0,0 +1,13 @@
+package com.example.smart_waiting;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.BDDMockito.given;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+public class MarketServiceTest {
+}
