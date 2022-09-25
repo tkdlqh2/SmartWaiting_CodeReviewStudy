0a7f817b3493930d547050048e161bbe39b36cfa
tkdlqh2 <yhj7124@naver.com>
2022-09-25 17:06:54 +0900

    FileUtils class 추가

---
+++ src/main/java/com/example/smart_waiting/util/FileUtils.java
@@ -0,0 +1,84 @@
+package com.example.smart_waiting.util;
+
+import com.example.smart_waiting.domain.InputWithFile;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Slf4j
+public class FileUtils {
+
+    @Value("${file.local_path}")
+    private static String BASE_LOCAL_PATH;
+
+    @Value("${file.url_path}")
+    private static String BASE_URL_PATH;
+
+    public static void SetFileNames(MultipartFile file, InputWithFile input){
+
+        String saveFilename = "";
+        String urlFilename = "";
+
+        if (file != null) {
+            String originalFilename = file.getOriginalFilename();
+
+            String[] arrFilename = getNewSaveFile(BASE_LOCAL_PATH, BASE_URL_PATH, originalFilename);
+
+            saveFilename = arrFilename[0];
+            urlFilename = arrFilename[1];
+
+            try {
+                File newFile = new File(saveFilename);
+                FileCopyUtils.copy(file.getInputStream(), new FileOutputStream(newFile));
+            } catch (IOException e) {
+                log.info(e.getMessage());
+            }
+        }
+        input.setOriginalImageName(saveFilename);
+        input.setImagePath(urlFilename);
+    }
+
+    private static String[] getNewSaveFile(String baseLocalPath, String baseUrlPath, String originalFilename) {
+
+        LocalDate now = LocalDate.now();
+
+        String[] dirs = {
+                String.format("%s/%d/", baseLocalPath,now.getYear()),
+                String.format("%s/%d/%02d/", baseLocalPath, now.getYear(),now.getMonthValue()),
+                String.format("%s/%d/%02d/%02d/", baseLocalPath, now.getYear(), now.getMonthValue(), now.getDayOfMonth())};
+
+        String urlDir = String.format("%s/%d/%02d/%02d/", baseUrlPath, now.getYear(), now.getMonthValue(), now.getDayOfMonth());
+
+        for(String dir : dirs) {
+            File file = new File(dir);
+            if (!file.isDirectory()) {
+                file.mkdir();
+            }
+        }
+
+        String fileExtension = "";
+        if (originalFilename != null) {
+            int dotPos = originalFilename.lastIndexOf(".");
+            if (dotPos > -1) {
+                fileExtension = originalFilename.substring(dotPos + 1);
+            }
+        }
+
+        String uuid = UUID.randomUUID().toString().replaceAll("-", "");
+        String newFilename = String.format("%s%s", dirs[2], uuid);
+        String newUrlFilename = String.format("%s%s", urlDir, uuid);
+        if (fileExtension.length() > 0) {
+            newFilename += "." + fileExtension;
+            newUrlFilename += "." + fileExtension;
+        }
+
+        return new String[]{newFilename, newUrlFilename};
+    }
+}
