e89054854e64fa0bfd6f10cb547be5eb818d8295
tkdlqh2 <yhj7124@naver.com>
2022-09-26 04:15:21 +0900

    ServiedResult class 삭제 -> 오류가 생기면 exception을 던지는 것으로 충분함

--- src/main/java/com/example/smart_waiting/user/service/UserService.java
+++ src/main/java/com/example/smart_waiting/user/service/UserService.java
@@ -1,7 +1,5 @@
 package com.example.smart_waiting.user.service;
 
-import com.example.smart_waiting.domain.ServiceResult;
-import com.example.smart_waiting.user.User;
 import com.example.smart_waiting.user.model.UserDto;
 import com.example.smart_waiting.user.model.UserInput;
 import com.example.smart_waiting.user.model.UserLoginInput;
@@ -14,19 +12,19 @@ import javax.servlet.http.HttpServletRequest;
 @Service
 public interface UserService extends UserDetailsService {
 
-    ServiceResult createUser(UserInput userInput);
+    void createUser(UserInput userInput);
 
     boolean existEmail(String email);
 
     boolean existPhone(String phone);
 
-    ServiceResult emailAuth(String uuid);
+    void emailAuth(String uuid);
 
     UserDto login(UserLoginInput parameter);
 
     UserDto findFromRequest(HttpServletRequest request);
 
-    ServiceResult updateInfo(String email,UserInput parameter);
+    void updateInfo(String email,UserInput parameter);
 
-    ServiceResult updatePassword(String email, UserPasswordResetInput parameter);
+    void updatePassword(String email, UserPasswordResetInput parameter);
 }
--- src/main/java/com/example/smart_waiting/user/service/UserServiceImpl.java
+++ src/main/java/com/example/smart_waiting/user/service/UserServiceImpl.java
@@ -1,9 +1,9 @@
 package com.example.smart_waiting.user.service;
 
 import com.example.smart_waiting.components.MailComponents;
-import com.example.smart_waiting.domain.ServiceResult;
-import com.example.smart_waiting.exception.PasswordNotMatchException;
+import com.example.smart_waiting.exception.UserException;
 import com.example.smart_waiting.security.TokenUtil;
+import com.example.smart_waiting.type.ErrorCode;
 import com.example.smart_waiting.type.UserStatus;
 import com.example.smart_waiting.user.User;
 import com.example.smart_waiting.user.UserRepository;
@@ -20,11 +20,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.swing.text.html.Option;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -35,11 +33,10 @@ public class UserServiceImpl implements UserService{
     private final PasswordEncoder passwordEncoder;
     @Autowired
     private final MailComponents mailComponents;
-    private final TokenUtil tokenUtil;
 
     @Transactional
     @Override
-    public ServiceResult createUser(UserInput userInput) {
+    public void createUser(UserInput userInput) {
 
         String encryptPassword = passwordEncoder.encode(userInput.getPassword());
         String uuid = UUID.randomUUID().toString();
@@ -64,8 +61,6 @@ public class UserServiceImpl implements UserService{
         String text = "<p>Smart Waiting 사이트 가입을 축하드립니다.<p><p>아래 링크를 클릭하셔서 가입을 완료 하세요.</p>"
                 + "<div><a target='_blank' href='http://localhost:8080/user/email-auth?id=" + uuid + "'> 가입 완료 </a></div>";
         mailComponents.sendMail(email, subject, text);
-
-        return ServiceResult.success();
     }
 
     @Transactional(readOnly = true)
@@ -83,18 +78,13 @@ public class UserServiceImpl implements UserService{
     // 회원 가입 신청을 넣고 인증기한이 만료된 사용자에 대한 처리 필요!
     @Transactional
     @Override
-    public ServiceResult emailAuth(String uuid) {
-        Optional<User> optionalUser = userRepository.findByAuthKey(uuid);
-        if(optionalUser.isEmpty()){
-            // 인증키가 유효하지 않음!
-            return ServiceResult.fail("인증키가 유효하지 않습니다.");
-        }
+    public void emailAuth(String uuid) {
 
-        User user = optionalUser.get();
+        User user = userRepository.findByAuthKey(uuid).orElseThrow(
+                ()-> new UserException(ErrorCode.REG_KEY_INVALIDATE));
 
         if(user.getAuthDate().isBefore(LocalDateTime.now())){
-            // 만료된 인증키!
-            return ServiceResult.fail("인증키가 만료되었습니다.");
+            throw new UserException(ErrorCode.REG_KEY_EXPIRED);
         }
 
         user.setUserStatus(UserStatus.APPROVED);
@@ -102,7 +92,6 @@ public class UserServiceImpl implements UserService{
         user.setAuthDate(null);
         userRepository.save(user);
 
-        return ServiceResult.success();
     }
 
     @Override
@@ -118,7 +107,7 @@ public class UserServiceImpl implements UserService{
                 ()-> new UsernameNotFoundException("이메일이 없습니다. -> "+parameter.getEmail()));
 
         if(!passwordEncoder.matches(parameter.getPassword(), user.getPassword())){
-            throw new PasswordNotMatchException();
+            throw new UserException(ErrorCode.PASSWORD_NOT_MATCH);
         }
         System.out.println("로그인을 하였습니다.");
         return UserDto.of(user);
@@ -126,42 +115,34 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public UserDto findFromRequest(HttpServletRequest request) {
-        String email = tokenUtil.getEmail(tokenUtil.resolveTokenFromRequest(request));
+        String email = TokenUtil.getEmail(TokenUtil.resolveTokenFromRequest(request));
         User user = userRepository.findByEmail(email).orElseThrow(
                 ()-> new UsernameNotFoundException("이메일이 없습니다. -> "+email));
         return UserDto.of(user);
     }
 
     @Override
-    public ServiceResult updateInfo(String email, UserInput parameter) {
-        Optional<User> optionalUser = userRepository.findByEmail(email);
-        if(optionalUser.isEmpty()){
-            return ServiceResult.fail("회원 정보가 존재하지 않습니다.");
-        }
-        User user = optionalUser.get();
+    public void updateInfo(String email, UserInput parameter) {
+
+        User user = userRepository.findByEmail(email).orElseThrow(
+                ()->new UsernameNotFoundException("이메일이 없습니다. -> "+email));
         user.setPhone(parameter.getPhone());
         userRepository.save(user);
-
-        return ServiceResult.success();
     }
 
 
     @Override
-    public ServiceResult updatePassword(String email, UserPasswordResetInput parameter) {
-        Optional<User> optionalUser = userRepository.findByEmail(email);
-        if(optionalUser.isEmpty()){
-            return ServiceResult.fail("회원 정보가 존재하지 않습니다.");
-        }
-        User user = optionalUser.get();
+    public void updatePassword(String email, UserPasswordResetInput parameter) {
+
+        User user = userRepository.findByEmail(email).orElseThrow(
+                ()->new UsernameNotFoundException("이메일이 없습니다. -> "+email));
 
         if(!passwordEncoder.matches(parameter.getPassword(), user.getPassword())){
-            return ServiceResult.fail("현재 비밀번호가 일치하지 않습니다.");
+            throw new UserException(ErrorCode.PASSWORD_NOT_MATCH);
         }
 
         String encryptedPassword = passwordEncoder.encode(parameter.getNewPassword());
         user.setPassword(encryptedPassword);
         userRepository.save(user);
-
-        return ServiceResult.success();
     }
 }
