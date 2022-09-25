da774f25d07dda44b9668bfc545ce18e336a581c
tkdlqh2 <yhj7124@naver.com>
2022-09-26 04:33:56 +0900

    UserException 수정 & controller에서 ServiceResult class 제거

--- src/main/java/com/example/smart_waiting/exception/UserException.java
+++ src/main/java/com/example/smart_waiting/exception/UserException.java
@@ -5,15 +5,13 @@ import lombok.*;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class UserException extends RuntimeException{
     private ErrorCode errorCode;
-    private String errorMessage;
 
     public UserException(ErrorCode errorCode){
+        super(errorCode.getDescription());
         this.errorCode = errorCode;
-        this.errorMessage = errorCode.getDescription();
     }
 }
