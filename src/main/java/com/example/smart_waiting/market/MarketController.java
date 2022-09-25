3c7549af68790b60883a3830b9ad569ae5f5f167
tkdlqh2 <yhj7124@naver.com>
2022-09-25 15:46:18 +0900

    reg method 추가

--- src/main/java/com/example/smart_waiting/market/MarketController.java
+++ src/main/java/com/example/smart_waiting/market/MarketController.java
@@ -1,15 +1,35 @@
 package com.example.smart_waiting.market;
 
+import com.example.smart_waiting.market.model.MarketRegInput;
+import com.example.smart_waiting.market.service.MarketService;
+import com.example.smart_waiting.user.model.UserDto;
+import com.example.smart_waiting.user.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
+@RequiredArgsConstructor
 @Controller
 @RequestMapping("/market")
 public class MarketController {
 
-    @GetMapping("reg-form")
+//    private final UserService userService;
+    private final MarketService marketService;
+
+    @GetMapping("/reg-form")
     public String regForm(){
         return "market/reg_form";
+    }
+
+    @PostMapping("/reg")
+    public String reg(HttpServletRequest request, MarketRegInput parameter){
+//        UserDto userDto = userService.findFromRequest(request);
+        marketService.regMarket(userDto, parameter);
+
+        return "";
     }
 }
