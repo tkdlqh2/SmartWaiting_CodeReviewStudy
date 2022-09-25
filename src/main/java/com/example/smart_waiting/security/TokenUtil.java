1f87809e4b04dc44cf42557198ddfb532c9edb2a
tkdlqh2 <yhj7124@naver.com>
2022-09-26 03:39:21 +0900

    fix: TokenUtil을 component 에서 static method를 담은 class로 변경

--- src/main/java/com/example/smart_waiting/security/TokenUtil.java
+++ src/main/java/com/example/smart_waiting/security/TokenUtil.java
@@ -4,35 +4,33 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
-@Component
 public class TokenUtil {
 
     public static final String TOKEN_HEADER = "Authorization";
     public static final String TOKEN_PREFIX = "Bearer ";
 
     @Value("{spring.jwt.secret}")
-    private String secretKey;
+    private static String secretKey;
 
 
-    public String getEmail(String token){
-        return this.parseClaims(token).getSubject();
+    public static String getEmail(String token){
+        return TokenUtil.parseClaims(token).getSubject();
     }
 
-    public boolean validateToken(String token){
+    public static boolean validateToken(String token){
         if(!StringUtils.hasText(token)) return false;
 
-        var claims = this.parseClaims(token);
+        var claims = TokenUtil.parseClaims(token);
         return !claims.getExpiration().before(new Date());
     }
 
-    private Claims parseClaims(String token){
+    private static Claims parseClaims(String token){
         try{
             return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
         } catch (ExpiredJwtException e){
