6f2ced4f4f37b87e5f495af7e6ece20959686554
tkdlqh2 <yhj7124@naver.com>
2022-09-26 04:12:19 +0900

    기존 Exception에서 ErrorCode enum class 추가 & 그를 활용한 UserException 생성

--- src/main/java/com/example/smart_waiting/exception/UserException.java
+++ src/main/java/com/example/smart_waiting/exception/UserException.java
@@ -5,13 +5,25 @@ import lombok.*;
 
 @Getter
 @Setter
+<<<<<<< HEAD
+=======
+@AllArgsConstructor
+>>>>>>> 7a3644f (기존 Exception에서 ErrorCode enum class 추가 & 그를 활용한 UserException 생성)
 @NoArgsConstructor
 @Builder
 public class UserException extends RuntimeException{
     private ErrorCode errorCode;
+<<<<<<< HEAD
 
     public UserException(ErrorCode errorCode){
         super(errorCode.getDescription());
         this.errorCode = errorCode;
+=======
+    private String errorMessage;
+
+    public UserException(ErrorCode errorCode){
+        this.errorCode = errorCode;
+        this.errorMessage = errorCode.getDescription();
+>>>>>>> 7a3644f (기존 Exception에서 ErrorCode enum class 추가 & 그를 활용한 UserException 생성)
     }
 }
