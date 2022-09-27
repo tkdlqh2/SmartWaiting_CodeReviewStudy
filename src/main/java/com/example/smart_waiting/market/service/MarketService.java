9b067789ed464ce8ee24b8865e993e9eebe726f2
tkdlqh2 <yhj7124@naver.com>
2022-09-27 15:31:12 +0900

    fix : MarketService의 method 반환 타입에서 ServiceResult 삭제

--- src/main/java/com/example/smart_waiting/market/service/MarketService.java
+++ src/main/java/com/example/smart_waiting/market/service/MarketService.java
@@ -1,20 +1,16 @@
 package com.example.smart_waiting.market.service;
 
-import com.example.smart_waiting.domain.ServiceResult;
 import com.example.smart_waiting.market.model.MarketDto;
 import com.example.smart_waiting.market.model.MarketInfoInput;
 import com.example.smart_waiting.market.model.MarketRegInput;
-import com.example.smart_waiting.user.model.UserDto;
 import org.springframework.stereotype.Service;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Service
 public interface MarketService {
 
-    ServiceResult regMarket(MarketDto marketDto, MarketRegInput parameter);
+    void regMarket(String email, MarketRegInput parameter);
 
-    ServiceResult editInfo(MarketDto marketDto, MarketInfoInput parameter);
+    void editInfo(String email, MarketInfoInput parameter);
 
-    MarketDto findFromRequest(HttpServletRequest request);
+    MarketDto findDtoFromEmail(String email);
 }
--- src/main/java/com/example/smart_waiting/market/service/MarketServiceImpl.java
+++ src/main/java/com/example/smart_waiting/market/service/MarketServiceImpl.java
@@ -1,24 +1,20 @@
 package com.example.smart_waiting.market.service;
 
-import com.example.smart_waiting.domain.ServiceResult;
-import com.example.smart_waiting.exception.MarketNotFoundException;
+import com.example.smart_waiting.exception.MarketException;
 import com.example.smart_waiting.market.FoodRepository;
 import com.example.smart_waiting.market.Market;
 import com.example.smart_waiting.market.MarketRepository;
 import com.example.smart_waiting.market.model.MarketDto;
 import com.example.smart_waiting.market.model.MarketInfoInput;
 import com.example.smart_waiting.market.model.MarketRegInput;
+import com.example.smart_waiting.type.ErrorCode;
 import com.example.smart_waiting.type.MarketStatus;
 import com.example.smart_waiting.type.MarketType;
 import com.example.smart_waiting.user.User;
 import com.example.smart_waiting.user.UserRepository;
-import com.example.smart_waiting.user.model.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -28,16 +24,15 @@ public class MarketServiceImpl implements MarketService{
     private final FoodRepository foodRepository;
     private final UserRepository userRepository;
 
-    @Override
-    public ServiceResult regMarket(MarketDto marketDto, MarketRegInput parameter) {
-        Optional<User> optionalUser = userRepository.findByEmail(userDto.getEmail());
-        if(optionalUser.isEmpty()){
-            return ServiceResult.fail("유저가 존재하지 않습니다. -> "+userDto.getEmail());
-        }
 
+    @Override
+    public void regMarket(String email, MarketRegInput parameter) {
+
+        User user = userRepository.findByEmail(email).orElseThrow(
+                ()-> new UsernameNotFoundException("유저가 존재하지 않습니다. -> "+ email));
 
         Market market = Market.builder()
-                .owner(optionalUser.get())
+                .owner(user)
                 .name(parameter.getName())
                 .registrationNum(parameter.getRegistrationNum())
                 .zipCode(parameter.getZipCode())
@@ -47,31 +42,30 @@ public class MarketServiceImpl implements MarketService{
 
         marketRepository.save(market);
 
-        return ServiceResult.success();
     }
 
     @Override
-    public ServiceResult editInfo(MarketDto marketDto, MarketInfoInput parameter) {
-        Optional<User> optionalUser = userRepository.findByEmail(userDto.getEmail());
-        if(optionalUser.isEmpty()){return ServiceResult.fail("유저가 존재하지 않습니다. ->"+userDto.getEmail());}
+    public void editInfo(String email, MarketInfoInput parameter) {
+        Market market = findFromEmail(email);
 
-        Optional<Market> optionalMarket = marketRepository.findByUser(optionalUser.get());
-        if(optionalMarket.isEmpty()){return ServiceResult.fail("음식점을 등록하지 않은 유저입니다.");}
-
-        Market market = optionalMarket.get();
         market.setOpenHour(parameter.getOpenHour());
         market.setCloseHour(parameter.getCloseHour());
         market.setOpenWeekDay(parameter.getOpenWeekDay());
         market.setMaximumRegNum(parameter.getMaximumRegNum());
         market.setImagePath(parameter.getImagePath());
         market.setOpen(parameter.isOpen());
-
-        return ServiceResult.success();
     }
 
+    @Override
+    public MarketDto findDtoFromEmail(String email) {
+        return MarketDto.of(findFromEmail(email));
+    }
 
-    public MarketDto findFromRequest(HttpServletRequest request) {
+    private Market findFromEmail(String email){
+        User user = userRepository.findByEmail(email).orElseThrow(
+                ()-> new UsernameNotFoundException("유저가 존재하지 않습니다. ->"+ email));
 
-        return null;
+        return marketRepository.findByUser(user).orElseThrow(
+                ()-> new MarketException(ErrorCode.NO_MARKET_USER));
     }
 }
